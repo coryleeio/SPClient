c63ea1bff2851a1ef88f3244c26e564c04165fba
Cory Lee <cl10001123@gmail.com>
2015-08-25 02:04:40 -0500

    Added ECS startup test to verify timezone fix is working in unity5

---
+++ Assets/PhysicsTest/ECSTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Artemis;
+
+
+public class ECSTest : MonoBehaviour {
+	EntityWorld world;
+
+
+	void Start () {
+		world = new EntityWorld ();
+
+	}
+	
+	void Update () {
+		if (world != null) {
+			world.Update();
+		}
+	}
+}
